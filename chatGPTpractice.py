af2aa84dc3b44622065ef2c33015f10152b638b8
bryce.patterson@gmail.com <bryce.patterson@gmail.com>
2024-06-26 20:57:48 -0400

    updated v0.0.1

--- chatGPTpractice.py
+++ chatGPTpractice.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+class Dog:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        return f"{self.name} says woof!"
+        
+my_dog = Dog("Buddy", 3)
+print(my_dog.bark())
